7164007131adb72b15a3366223ea2fdd6148e672
khanhtran <kaitran2205@gmail.com>
2025-01-12 20:58:17 +0700

    changed

--- src/main/java/com/hss/HssApplication.java
+++ src/main/java/com/hss/HssApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HssApplication {
     public static void main(String[] args) {
+        String name;
         SpringApplication.run(HssApplication.class, args);
     }
 } 
