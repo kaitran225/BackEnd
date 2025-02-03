bafd5761350d8fa4d07dc95f72583d88648907be
kaitran2205 <kaitran2205@gmail.com>
2025-02-03 17:59:27 +0700

    merge

--- src/main/java/com/healthy/BackEnd/Exception/ErrorResponse.java
+++ src/main/java/com/healthy/BackEnd/Exception/ErrorResponse.java
@@ -1,32 +1,13 @@
-package com.healthy.BackEnd.exception;
+package com.healthy.BackEnd.Exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ErrorResponse  {
     private String message;
     private int status;
-
-    public ErrorResponse(String message,int status) {
-        this.message = message;
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-
-
-
-
 }
