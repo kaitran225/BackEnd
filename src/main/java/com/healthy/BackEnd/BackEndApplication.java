8dff90fd21e38a512699a4f6fdf1f5f67f473aa7
kaitran2205 <kaitran2205@gmail.com>
2025-02-08 03:50:13 +0700

    reset

--- src/main/java/com/healthy/BackEnd/BackEndApplication.java
+++ src/main/java/com/healthy/BackEnd/BackEndApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
     public static void main(String[] args) {
+        System.out.println("url" + System.getenv("DATABASE_URL"));
         SpringApplication.run(BackEndApplication.class, args);
     }
 
