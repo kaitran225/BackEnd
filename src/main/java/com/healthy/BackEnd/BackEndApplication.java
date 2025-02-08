82e9d8db0f4c3f2047cc51562e379910df558d50
kaitran2205 <kaitran2205@gmail.com>
2025-02-08 17:50:36 +0700

    save

--- src/main/java/com/healthy/BackEnd/BackEndApplication.java
+++ src/main/java/com/healthy/BackEnd/BackEndApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
     public static void main(String[] args) {
-        System.out.println("url" + System.getenv("DATABASE_URL"));
         SpringApplication.run(BackEndApplication.class, args);
     }
 
