7e0d0b3bb17f03cfa7531673165a305aa8180391
anhCuong <cuongcaoleanh@gmail.com>
2025-03-04 17:50:47 +0700

    updateAuthorizeManagerController

--- src/main/java/com/healthy/backend/controller/ManagerController.java
+++ src/main/java/com/healthy/backend/controller/ManagerController.java
@@ -5,9 +5,13 @@ package com.healthy.backend.controller;
 
 import com.healthy.backend.dto.manager.AppointmentStatsResponse;
 import com.healthy.backend.dto.manager.PsychologistStatsResponse;
+import com.healthy.backend.entity.Users;
+import com.healthy.backend.enums.Role;
+import com.healthy.backend.security.TokenService;
 import com.healthy.backend.service.ManagerService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
@@ -21,16 +25,26 @@ import java.util.List;
 @Tag(name = "Manager Controller", description = "Manager specific APIs")
 public class ManagerController {
     private final ManagerService managerService;
-
+    private  final TokenService tokenService;
     // Endpoint to get appointment statistics by status
     @GetMapping("/stats/appointments")
-    public AppointmentStatsResponse getAppointmentStats() {
+    public AppointmentStatsResponse getAppointmentStats(
+            HttpServletRequest httpRequest
+    ) {
+        if (!tokenService.validateRole(httpRequest, Role.MANAGER) ) {
+            throw new IllegalArgumentException("Unauthorized access get Appointments ");
+        }
+
         return managerService.getAppointmentStats();
     }
 
-    // Endpoint to get psychologist statistics (average rating and number of appointments)
     @GetMapping("/stats/psychologists")
-    public List<PsychologistStatsResponse> getPsychologistStats() {
+    public List<PsychologistStatsResponse> getPsychologistStats(
+            HttpServletRequest httpRequest
+    ) {
+        if (!tokenService.validateRole(httpRequest, Role.MANAGER) ) {
+            throw new IllegalArgumentException("Unauthorized access get Appointments ");
+        }
         return managerService.getPsychologistStats();
     }
 
