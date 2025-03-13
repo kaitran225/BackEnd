a775334293467614ebc388b301156c1d208d4901
khanhtran <kaitran2205@gmail.com>
2025-03-13 09:06:12 +0700

    performance

--- src/main/java/com/healthy/backend/repository/ResetTokenRepository.java
+++ src/main/java/com/healthy/backend/repository/ResetTokenRepository.java
@@ -1,13 +1,13 @@
 package com.healthy.backend.repository;
 
 import com.healthy.backend.entity.ResetToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
 @Repository
