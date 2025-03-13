6b3314f9f81feff9709e707d1a3b312ad4f644be
khanhtran <kaitran2205@gmail.com>
2025-03-13 22:30:13 +0700

    shutdown

--- src/main/java/com/healthy/backend/config/SystemConfig.java
+++ src/main/java/com/healthy/backend/config/SystemConfig.java
@@ -1,15 +1,25 @@
 package com.healthy.backend.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Component
 public class SystemConfig {
 
 //    @Scheduled(cron = "0 0 12 * * ?")
-    @Scheduled(cron = "0 22 10 * * ?")
+    @Scheduled(cron = "0 32 10 * * ?")
     public void shutdownAtNoon() {
         System.out.println("Shutting down the application at noon...");
         System.exit(0);
+    }
+
+    @PostConstruct
+    public void logTimezone() {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println("Current server time: " + now);
+        System.out.println("Current system timezone: " + ZoneId.systemDefault());
     }
 }
