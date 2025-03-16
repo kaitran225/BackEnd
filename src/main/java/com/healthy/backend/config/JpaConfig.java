638f381d23ec3e5c640974565248e7ba7f84835a
kaitran2205 <kaitran2205@gmail.com>
2025-03-16 17:30:14 +0700

    jpa config 2

--- src/main/java/com/healthy/backend/config/JpaConfig.java
+++ src/main/java/com/healthy/backend/config/JpaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.core.Ordered;
+import org.springframework.web.filter.RequestContextFilter;
 
 @Configuration
 public class JpaConfig {
@@ -12,8 +13,20 @@ public class JpaConfig {
     @Bean
     public FilterRegistrationBean<OpenEntityManagerInViewFilter> openEntityManagerInViewFilter() {
         FilterRegistrationBean<OpenEntityManagerInViewFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new OpenEntityManagerInViewFilter());
+        OpenEntityManagerInViewFilter filter = new OpenEntityManagerInViewFilter();
+        filter.setEntityManagerFactoryBeanName("entityManagerFactory");
+        filterRegistrationBean.setFilter(filter);
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestContextFilter> requestContextFilter() {
+        FilterRegistrationBean<RequestContextFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new RequestContextFilter());
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
 } 
