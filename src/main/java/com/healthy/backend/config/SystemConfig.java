410d4a7593ed3e7877d18bc80e5aabc76c873981
Kai Trần <69950000+kaitran225@users.noreply.github.com>
2025-03-17 00:22:56 +0700

    Update SystemConfig.java

--- src/main/java/com/healthy/backend/config/SystemConfig.java
+++ src/main/java/com/healthy/backend/config/SystemConfig.java
@@ -1,26 +1,14 @@
 package com.healthy.backend.config;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 @Component
 public class SystemConfig {
 
-//    @Scheduled(cron = "0 0 12 * * ?")
-    @Scheduled(cron = "0 50 22 * * ?")
+    @Scheduled(cron = "0 30 0 * * ?")
     public void shutdownAtNoon() {
         System.out.println("Shutting down the application at noon...");
         System.exit(0);
-    }
-
-    @PostConstruct
-    public void logTimezone() {
-        ZonedDateTime now = ZonedDateTime.now();
-        System.out.println("Current server time: " + now);
-        System.out.println("Current system timezone: " + ZoneId.systemDefault());
     }
 }
