e14672d8674e9b83fa5c41da33733fb834979f54
kaitran2205 <kaitran2205@gmail.com>
2025-03-17 14:00:10 +0700

    construct

--- src/main/java/com/healthy/backend/dto/programs/ProgramUpdateRequest.java
+++ src/main/java/com/healthy/backend/dto/programs/ProgramUpdateRequest.java
@@ -1,13 +1,16 @@
 package com.healthy.backend.dto.programs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProgramUpdateRequest extends ProgramsRequest {
 
