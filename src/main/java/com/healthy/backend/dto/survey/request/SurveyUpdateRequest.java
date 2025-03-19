43fd908a9a5c6674a1e4965e637e9edd01a51d19
kaitran2205 <kaitran2205@gmail.com>
2025-03-19 19:45:56 +0700

    save

--- src/main/java/com/healthy/backend/dto/survey/request/SurveyUpdateRequest.java
+++ src/main/java/com/healthy/backend/dto/survey/request/SurveyUpdateRequest.java
@@ -30,7 +30,6 @@ public class SurveyUpdateRequest {
     @Valid
     @Schema(example = "2025-01-01")
     private String startDate;
-    @Valid
     @Pattern(regexp = "^[1-9]$", message = "Periodic must be a number between 1 and 9")
     @Schema(example = "1")
     private int periodic;
